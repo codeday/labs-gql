55e6d81a5484aebe9c4c931e5e5a57c7aca19857
Tyler Menezes <tylermenezes@codeday.org>
2021-06-04 11:13:22 -0400

    Add student filtering by name and email

--- src/inputs/StudentFilterInput.ts
+++ src/inputs/StudentFilterInput.ts
@@ -13,11 +13,23 @@ export class StudentFilterInput {
   @Field(() => String, { nullable: true })
   partnerCode?: string
 
+  @Field(() => String, { nullable: true })
+  givenName?: string
+
+  @Field(() => String, { nullable: true })
+  surname?: string
+
+  @Field(() => String, { nullable: true })
+  email?: string
+
   toQuery(): Prisma.StudentWhereInput {
     return {
       status: this.inStatus,
       partnerCode: this.partnerCode,
       projects: this.withProjects ? { some: {} } : undefined,
+      givenName: this.givenName,
+      surname: this.surname,
+      email: this.email,
     };
   }
 }
